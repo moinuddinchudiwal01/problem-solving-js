20900f2d4ed782710b418b887f70484591e7aa67
Moinuddin <moinuddinchudiwal@gmail.com>
2024-12-24 09:08:06 +0530

    star pattern problem solving

---
+++ star patterns programs/6-10Answer.js
@@ -0,0 +1,20 @@
+// 6.Print a rectangle of * with a specified width and height.
+// *****
+// *****
+// *****
+// Answer:
+// const RectangleStarPattern = (h,w) =>{
+//     let ans = "";
+
+//     for(let i =0;i<h;i++){
+//         ans += "*";
+//         for(let j = 0;j<w-1;j++){
+//             ans += "*";
+//         }
+//         ans +="\n";
+//     }
+
+//     return ans;
+// }
+
+// console.log(RectangleStarPattern(3,5))
